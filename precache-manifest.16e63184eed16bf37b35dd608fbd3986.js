4f01d7c3a4d971dfbf599a51db54368ce57faf8d
Younis A M <myounis.ar@live.com>
2019-01-27 19:24:35 +0530

    Updates

---
+++ precache-manifest.16e63184eed16bf37b35dd608fbd3986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "377f3f2a0d1ee2473f68",
+    "url": "/React-To-Do/static/js/runtime~main.377f3f2a.js"
+  },
+  {
+    "revision": "0f98861c41bea047c0e2",
+    "url": "/React-To-Do/static/js/main.0f98861c.chunk.js"
+  },
+  {
+    "revision": "4ccf09c9ceb1e6113655",
+    "url": "/React-To-Do/static/js/1.4ccf09c9.chunk.js"
+  },
+  {
+    "revision": "0f98861c41bea047c0e2",
+    "url": "/React-To-Do/static/css/main.fbb942d5.chunk.css"
+  },
+  {
+    "revision": "e28361b02889a422af1f080ebb415c7f",
+    "url": "/React-To-Do/index.html"
+  }
+];
